08df939cf9b89ad9c467ad4303f05489fbfe1aec
Snacks <Armadillo@Armadillo.local>
2012-03-28 21:07:27 -0700

    added a placeholder thumbnail to the balloon

--- gen/net/winterroot/android/wildflowers/R.java
+++ gen/net/winterroot/android/wildflowers/R.java
@@ -15,6 +15,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
         public static final int map_device_user_point=0x7f020002;
         public static final int mappoint=0x7f020003;
+        public static final int test=0x7f020004;
     }
     public static final class id {
         public static final int cameraButton=0x7f050005;

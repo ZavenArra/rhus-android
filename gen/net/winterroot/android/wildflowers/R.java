48597d8d517b0d2ed24ca3682e13e71e1eb50d49
Snacks <Armadillo@Armadillo.local>
2012-03-29 15:28:17 -0700

    added a camera button

--- gen/net/winterroot/android/wildflowers/R.java
+++ gen/net/winterroot/android/wildflowers/R.java
@@ -11,11 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int close=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int map_device_user_point=0x7f020002;
-        public static final int mappoint=0x7f020003;
-        public static final int test=0x7f020004;
+        public static final int camera=0x7f020000;
+        public static final int close=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int map_device_user_point=0x7f020003;
+        public static final int mappoint=0x7f020004;
+        public static final int test=0x7f020005;
     }
     public static final class id {
         public static final int cameraButton=0x7f050005;

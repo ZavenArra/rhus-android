d35dd3f69af8f3c949548719adfe61bcc9bc2577
Deepwinter <deepwinter@winterroot.net>
2012-04-03 15:49:48 -0700

    Checkpiont

--- gen/net/winterroot/android/wildflowers/R.java
+++ gen/net/winterroot/android/wildflowers/R.java
@@ -11,17 +11,33 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int camera=0x7f020000;
-        public static final int close=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int map_device_user_point=0x7f020003;
-        public static final int mappoint=0x7f020004;
-        public static final int test=0x7f020005;
+        public static final int barbackground=0x7f020000;
+        public static final int camera=0x7f020001;
+        public static final int close=0x7f020002;
+        public static final int everyonedeselected=0x7f020003;
+        public static final int everyoneselected=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int logo=0x7f020007;
+        public static final int map=0x7f020008;
+        public static final int map_device_user_point=0x7f020009;
+        public static final int mappoint=0x7f02000a;
+        public static final int meveryone=0x7f02000b;
+        public static final int mydeselected=0x7f02000c;
+        public static final int myselected=0x7f02000d;
+        public static final int popbackground=0x7f02000e;
+        public static final int redbackground=0x7f02000f;
+        public static final int test=0x7f020010;
     }
     public static final class id {
-        public static final int cameraButton=0x7f050005;
+        public static final int cameraButton=0x7f050007;
         public static final int close_button=0x7f050002;
+        public static final int everyonesDataButton=0x7f05000a;
+        public static final int imageView1=0x7f050009;
+        public static final int mapActivityView=0x7f050005;
+        public static final int mapOptionsButton=0x7f050008;
         public static final int mapmain=0x7f050006;
+        public static final int myDataButton=0x7f05000b;
         public static final int note_label=0x7f050001;
         public static final int note_text=0x7f050003;
         public static final int thumbnail=0x7f050004;
@@ -31,10 +47,10 @@ public final class R {
         public static final int baloon=0x7f030000;
         public static final int main=0x7f030001;
         public static final int map=0x7f030002;
+        public static final int mapoptionsbaloon=0x7f030003;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int note=0x7f040002;
+        public static final int app_name=0x7f040000;
+        public static final int note=0x7f040001;
     }
 }
